d31342a1f16ffcab0cf23a121f5ea62fb89a3a2b
Ibtid <ibtid011@example.com>
2020-05-29 03:06:40 +0600

    Updates

---
+++ precache-manifest.ef1e4744791ce848d294961cd1c90c43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89233222f6310954a50bc96e430432b",
+    "url": "/chefs-deck-react/index.html"
+  },
+  {
+    "revision": "a1df5fcf1d7346d2c7ed",
+    "url": "/chefs-deck-react/static/css/main.ebe16224.chunk.css"
+  },
+  {
+    "revision": "61e8b92bdcf25508b60a",
+    "url": "/chefs-deck-react/static/js/2.f036ab22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chefs-deck-react/static/js/2.f036ab22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1df5fcf1d7346d2c7ed",
+    "url": "/chefs-deck-react/static/js/main.1df18afb.chunk.js"
+  },
+  {
+    "revision": "558304a6ecbfd687f150",
+    "url": "/chefs-deck-react/static/js/runtime-main.ceaae899.js"
+  }
+]);
